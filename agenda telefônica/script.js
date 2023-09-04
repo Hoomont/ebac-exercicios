e60e31e7f22510757b4eabc3e62dc0ba2816ac3e
Hoomont <xd1i@outlook.com>
2023-09-04 19:18:33 -0300

    Atualizando a funcionalidade da agenda

--- agenda telefônica/script.js
+++ agenda telefônica/script.js
@@ -1,20 +1,23 @@
-const form = document.querySelector("form");
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    // Obtém os dados do formulário
-    const nome = document.querySelector("input[name='nome']").value;
-    const telefone = document.querySelector("input[name='telefone']").value;
-
-    // Adiciona os dados à tabela de contatos
-    const contato = {
-        nome: nome,
-        telefone: telefone
-    };
-
-    const contatos = document.querySelector("table").querySelector("tbody");
-    contatos.innerHTML += `<tr>
-        <td>${nome}</td>
-        <td>${telefone}</td>
-    </tr>`;
-});
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("#formulario");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+    
+        // Obtém os dados do formulário
+        const nome = document.querySelector("input[name='nome']").value;
+        const telefone = document.querySelector("input[name='telefone']").value;
+    
+        // Adiciona os dados à tabela de contatos
+        const contato = {
+            nome: nome,
+            telefone: telefone
+        };
+    
+        const contatos = document.querySelector("table");
+        contatos.innerHTML += `<tr>
+            <td>${nome}</td>
+            <td>${telefone}</td>
+        </tr>`;
+    });
+    });
+    
